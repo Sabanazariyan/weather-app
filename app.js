be362585b26fd3216ca4ffda00706121a9de47d9
saba <mahdaneparsa@gmail.com>
2025-01-18 15:04:49 +0330

    add  css style and get api key

---
+++ app.js
@@ -0,0 +1,5 @@
+const BASE_URL =
+  "https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=20f45ed1904591737c8bdd5780e6759b&units=metric";
+
+const API_KEY = "20f45ed1904591737c8bdd5780e6759b";
+
